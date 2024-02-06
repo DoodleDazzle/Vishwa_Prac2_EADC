459bc8254aaae9e792de75bb8e4bb0252a9494e0
Vishwa panchal <vishwapanchal323@gmail.com>
2024-02-06 16:26:29 +0530

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hey there, This is Vishwa panchal');
+    res.send('Hey there, this is Vishwa panchal');
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
